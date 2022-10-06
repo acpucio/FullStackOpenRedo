9c8038f352defb11b7537f0b13fb426415ae96e3
Albert Pucio <acpucio@gmail.com>
2022-10-06 15:46:39 -0500

    More on promises and chaining

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const PersonForm = ({ onSubmit, newName, newNumber, handleNameChange,handleNumberChange }) => {
     
@@ -22,14 +22,23 @@ const PersonForm = ({ onSubmit, newName, newNumber, handleNameChange,handleNumbe
   )
 }
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+      console.log('promise fulfillled')
+      setPersons(response.data)
+      })
+  },[])
+
+  console.log('render', persons.length, 'persons')
+
+
 
   const addPerson = (event) => {
     event.preventDefault()
