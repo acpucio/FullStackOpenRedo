0410062cef24a149cfc2eb2b73c2bcc639ee81aa
Albert Pucio <acpucio@gmail.com>
2022-11-08 18:59:49 -0600

    4.2 Blog list,  step2

--- part4/app.js
+++ part4/app.js
@@ -3,14 +3,29 @@ const app = express()
 const cors = require('cors')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blog')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 const Blog = require('./models/blog')
 
+
 const mongoUrl = config.mongoUrl
+
+logger.info('connecting to', config.mongoUrl)
 mongoose.connect(mongoUrl)
+.then(() =>{ 
+  logger.info('connected to MongoDB')
+})
+.catch((error) => {
+  logger.error('error connecting to MongoDB', error.message)
+})
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
 
